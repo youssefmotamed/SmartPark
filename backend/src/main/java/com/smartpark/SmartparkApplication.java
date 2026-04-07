747773afc3bb94d01959065ed8810f80d30a5ac8
Youssef Ahmed <youssefmotamed.y@gmail.com>
2026-04-07 22:38:14 +0200

    initialized spring boot

---
+++ backend/src/main/java/com/smartpark/SmartparkApplication.java
@@ -0,0 +1,13 @@
+package com.smartpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartpark/SmartparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
